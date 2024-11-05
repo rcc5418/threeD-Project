4430bc5b87cfc3b98a4bcf72ac4c2472f87176a3
Reece Cullen <rcc5418@psu.edu>
2024-11-05 18:17:43 -0500

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 import './style.css'
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/dist/vite.svg'
